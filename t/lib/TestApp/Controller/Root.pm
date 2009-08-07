eb1a771f89ce5caee023f80fe8997e1cbff88744
Dan Dascalescu <bigbang7@gmail.com>
2009-08-07 11:08:35 +0000

    Added Root.pm to avoid '[error] No default action defined'

---
+++ t/lib/TestApp/Controller/Root.pm
@@ -0,0 +1,15 @@
+package TestApp::Controller::Root;
+
+use strict;
+use warnings;
+use parent 'Catalyst::Controller';
+
+__PACKAGE__->config->{namespace} = '';
+
+sub default :Path {
+    my ( $self, $c ) = @_;
+    $c->response->body( 'Page not found' );
+    $c->response->status(404);
+}
+
+1;
