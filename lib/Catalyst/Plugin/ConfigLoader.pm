a6d02f0e9965167c4ea95ed31899fdee978c646e
Brian Cassidy <bricas@cpan.org>
2010-06-30 17:33:06 +0000

    copyright year

--- lib/Catalyst/Plugin/ConfigLoader.pm
+++ lib/Catalyst/Plugin/ConfigLoader.pm
@@ -356,7 +356,7 @@ Work to this module has been generously sponsored by:
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2006-2009 by Brian Cassidy
+Copyright 2006-2010 by Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
